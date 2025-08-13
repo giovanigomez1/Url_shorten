b27fb0532f0151b527a4e69fd5da20135c258c75
Giovani <giovani@MacBook-Pro-de-Giovani.local>
2025-08-12 20:53:36 -0600

    ready to deploy

--- api/index.js
+++ api/index.js
@@ -1,5 +1,49 @@
 import serverless from 'serverless-http';
-import app from '../server/app.js';
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch'; // For Node.js <18; in Node 18+, you can use native fetch
+
+const app = express();
+// const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get('/api', (req, res) => {
+  res.json({
+    'Greetings':'hello'
+  })
+})
+
+
+app.post('/api/shorten', async (req, res) => {
+  const longURL = req.body.url;
+  console.log(longURL)
+
+  if (!longURL) {
+    return res.status(400).json({ error: 'Missing URL' });
+  }
+
+  try {
+    const response = await fetch('https://cleanuri.com/api/v1/shorten', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({ url: longURL })
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error('Error calling CleanURI:', err);
+    res.status(500).json({ error: 'Server error shortening URL' });
+  }
+});
+
 
 export default serverless(app)
 
